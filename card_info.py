be5b7a1eaa08d3abee19ee7ebaafa7e97bbc3eb5
laurentalacoque <laurent.alacoque@gmail.com>
2018-11-12 14:39:56 +0100

    removed older schema

--- card_info.py
+++ card_info.py
@@ -1,7 +1,7 @@
 ﻿# -*- coding: utf-8 -*-
-printhex = True
-printbin = True
-printstr = True
+printhex = False
+printbin = False
+printstr = False
 printalpha = False
 en1545_alpha4 = [ "-","A","B","C","D","E","F","G",
           "H","I","J","K","L","M","N","O",
@@ -119,128 +119,8 @@ transitions  = {
     7 : "changement (Sortie)"
 }
 
-schema = {
-    ":1000:1004":
-        [
-            {"length":8*8, "type": "ascii", "name":"tag"},
-            {"length":8*8, "type": "null", "name":"null"},
-        ],
-    ":2000:2004":
-        [
-            {"length":8*8, "type": "ascii", "name":"tag"},
-            {"length":8*8, "type": "null", "name":"null"},
-        ],
-    ":3100:3104":
-        [
-            {"length":8*8, "type": "ascii", "name":"tag"},
-            {"length":8*8, "type": "null", "name":"null"},
-        ],
-    ":3f04":
-        [
-            {"length":8*8, "type": "ascii", "name":"tag"},
-            {"length":8*8, "type": "null", "name":"null"},
-        ],
-    ":2000:2050":
-        [
-            {"length":4, "type": "int", "name":"count"},
-            {"length":24, "type": "bestcontract", "name":"contract"},
-            {"length":24, "type": "bin", "name":"contract"},
-            {"length":24, "type": "bin", "name":"contract"},
-            {"length":24, "type": "bin", "name":"contract"},
-            {"length":132, "type": "null", "name":"null"},
-        ],
-    ":2000:2030": #Warning : same as 2020
-        [
-            {"length":7, "type": "bitmap", "name":"bitmap","value":"1110111"},
-            {"length":8, "type": "network", "name":"provider"},
-            #{"length":50, "type": "bin", "name":"unknown2"},
-            {"length":16, "type": "hex", "name":"contract-fare"},
-            {"length":32, "type": "hex", "name":"contract-serial"},
-            #validity
-            {"length":2, "type": "bitmap", "name":"validity bitmap","value":"11"},
-            {"length":14, "type": "date", "name":"abostart"},
-            {"length":14, "type": "date", "name":"aboend"},
-            {"length":8, "type": "contractstatus", "name":"status"},
-            {"length":26, "type": "bin", "name":"unknown"},
-            {"length":14, "type": "date", "name":"sale-date"},
-            {"length":8, "type": "bin", "name":"unknown"},
-            {"length":8, "type": "int", "name":"country"},
-            {"length":8, "type": "network", "name":"sale-op"},
-        
-            {"length":68, "type": "null", "name":"null"},
-        ],
-    ":2000:2020": #Warning : same as 2030
-        [
-            {"length":7, "type": "bitmap", "name":"bitmap","value":"1110111"},
-            {"length":8, "type": "network", "name":"provider"},
-            #{"length":50, "type": "bin", "name":"unknown2"},
-            {"length":16, "type": "hex", "name":"contract-fare"},
-            {"length":32, "type": "hex", "name":"contract-serial"},
-            #validity
-            {"length":2, "type": "bitmap", "name":"validity bitmap","value":"11"},
-            {"length":14, "type": "date", "name":"abostart"},
-            {"length":14, "type": "date", "name":"aboend"},
-            {"length":8, "type": "contractstatus", "name":"status"},
-            {"length":26, "type": "bin", "name":"unknown"},
-            {"length":14, "type": "date", "name":"sale-date"},
-            {"length":8, "type": "bin", "name":"unknown"},
-            {"length":8, "type": "int", "name":"country"},
-            {"length":8, "type": "network", "name":"sale-op"},
-        
-            {"length":68, "type": "null", "name":"null"},
-        ],
-    ":2000:2010":
-        [
-            {"length":14, "type": "date", "name":"date"},
-            {"length":11, "type": "time", "name":"time"},
-            {"length":28, "type": "bitmap", "name":"event-bitmap","value":"1010000000000100100100010100"},
-            {"length":4, "type": "modality", "name":"modality"},
-            {"length":4, "type": "transition", "name":"transition"},
-            {"length":8, "type": "network", "name":"reseau"},
-            {"length":16, "type": "location", "name":"eventloc"},
-            {"length":16, "type": "int", "name":"routenumber"},
-            {"length":16, "type": "int", "name":"coachnumber"},
-            #{"length":10, "type": "bin", "name":"unknown2"},
-            {"length":5, "type": "int", "name":"contract-pointer"},
-            {"length":5, "type": "bitmap", "name":"event-data-bitmap","value":"10110"},
-            {"length":11, "type": "time", "name":"firsttime"},
-            {"length":1, "type": "bin", "name":"is-simulation"},
-            {"length":2, "type": "bin", "name":"direction"},
-            {"length":91, "type": "null", "name":"null"}
-        ],
-    ":2000:2001":
-        [
-            {"length":6, "type": "bin", "name":"app-version"},
-            {"length":7, "type": "bitmap", "name":"bitmap", "value":"0000111"},
-            {"length":12, "type": "bcd3", "name":"country"},
-            {"length":12, "type": "bcd3", "name":"networkid"},
-            {"length":8, "type": "network", "name":"issuer-network"},
-            {"length":14, "type": "date", "name":"validity"},
-            #{"length":98, "type": "bin", "name":"unknown3"},
-            #{"length":10, "type": "bin", "name":"unknown3"},
-            {"length":8, "type": "bitmap", "name":"holder-bitmap", "value":"11000010"},
-            {"length":2, "type": "bitmap", "name":"birth-bitmap", "value":"01"},
-            {"length":32, "type": "bcddate", "name":"date-of-birth"},
-            {"length":4, "type": "bitmap", "name":"profiles-number", "value":"0001"},
-            {"length":3, "type": "bitmap", "name":"profile-bitmap", "value":"110"},
-            {"length":8, "type": "hex", "name":"profile-id"},
-            {"length":14, "type": "date", "name":"profile-date"},
-            # holder data
-            {"length":12, "type": "bitmap", "name":"holder-bitmap", "value":"000000001011"},
-            {"length":4, "type": "cardstatus", "name":"card-status"},
-            {"length":4, "type": "bin", "name":"telereglement"},
-            {"length":6, "type": "bin", "name":"commercial-id"},
-            {"length":76, "type": "null", "name":"null"},
-        ]
-}
-#TODO remove this eventually
-best_contract_schema = [
-            {"length":3, "type": "bitmap", "name":"bc-bitmap", "value":"110"},
-            {"length":4, "type": "bin", "name":"bc-tariff-expl"},
-            {"length":8, "type": "hex", "name":"bc-tariff-type"},
-            {"length":4, "type": "int", "name":"bc-tariff-priority"},
-            {"length":5, "type": "int", "name":"bc-pointer"},
-]
+
+##### Intercode structures
 
 # Environment and holder informations (2001)
 environment_version = [
@@ -369,7 +249,8 @@ contract_schema2 = [
 
 #Simple counter
 simulated_counter_schema = [
-    {"length":24, "type": "int", "name":"remaining-journeys"}
+    {"length":24, "type": "int", "name":"remaining-journeys"},
+    {"length":52*8, "type": "hex", "name":"data"}
 ]
 
 best_contracts_schema = [
@@ -646,135 +527,12 @@ def hex2str(hexstring):
     string = re.sub('[\x00-\x20\x7f-\xff]','`',bastr)
     return " ".join(string)
 
-def parse_hexstring(hexstring, schema,prefix=""):
-    binstring = hex2bin(hexstring)
-    result = "" #prefix + binstring +"\n"
-    for token in schema:
-        try:
-                tokenlength = token["length"]
-                tokendata = binstring[0:tokenlength]
-                binstring = binstring[tokenlength:]
-                tokenname = token["name"]
-                tokentype = token["type"]
-                tokenvalue = ""
-                if tokentype == "int":
-                    tokenvalue = str(int(tokendata,2))
-                    result += prefix + "%s: %s\n"%(tokenname,tokenvalue)
-                elif tokentype == "cardstatus":
-                    tokenvalue = (int(tokendata,2))
-                    tokenvalue2 = card_status.get(tokenvalue,"Unknown")
-                    result += prefix + "%s: %s (%s)\n"%(tokenname,tokenvalue, tokenvalue2)
-                elif tokentype == "contractstatus":
-                    tokenvalue = (int(tokendata,2))
-                    tokenvalue2 = contract_status.get(tokenvalue,"Unknown")
-                    result += prefix + "%s: %s (%s)\n"%(tokenname,tokenvalue, tokenvalue2)
-                elif tokentype == "hex":
-                    tokenvalue = str(hex(int(tokendata,2)))[2:]
-                    result += prefix + "%s: %sh\n"%(tokenname,tokenvalue)
-                elif tokentype == "bitmap":
-                    #TODO we should implement this
-                    #here we only check if the bitmap is valid
-                    #and exit if it's not
-                    if tokendata == token["value"]:
-                        # right bitmap
-                        result += prefix + "\t%s: [%s]\n"%(tokenname,tokendata)
-                    else:
-                        # wrong bitmap, we're dead
-                        result += "!!!!!! Error : wrong bitmap %s (was expecting %s)\n"%(tokendata,token["value"])
-                        return result
-                elif tokentype == "bin":
-                    tokenvalue = ""
-                    bin0 = bin2alpha(tokendata)
-                    bin1 = bin2alpha(tokendata[1:])
-                    bin2 = bin2alpha(tokendata[2:])
-                    bin3 = bin2alpha(tokendata[3:])
-                    if printalpha and len(tokendata) > 4:
-                        result += prefix + "%s: %sb (%s / .%s / ..%s / ...%s)\n"%(tokenname,tokendata, bin0,bin1,bin2,bin3)
-                    else:
-                        result += prefix + "%s: %sb\n"%(tokenname,tokendata)
-                elif tokentype == "date":
-                    from datetime import date,timedelta
-                    orig = date(1997,1,1)
-                    eventdate = orig+timedelta(days = int(tokendata,2))
-                    tokenvalue = str(eventdate)
-                    result += prefix + "%s: %s\n"%(tokenname,tokenvalue)
-                elif tokentype == "time":
-                    mins = int(tokendata,2)
-                    hours = int(mins / 60)
-                    minutes = mins - 60 * hours
-                    tokenvalue = "%02d:%02d"%(hours,minutes)
-                    result += prefix + "%s: %s\n"%(tokenname,tokenvalue)
-                elif tokentype == "location":
-                    tokenvalue = location.get(int(tokendata,2),int(tokendata,2))
-                    result += prefix + "%s: %s\n"%(tokenname,tokenvalue)
-                elif tokentype == "zone":
-                    tokenvalue = zones.get(int(tokendata,2),int(tokendata,2))
-                    result += prefix + "%s: %s\n"%(tokenname,tokenvalue)
-                elif tokentype == "modality":
-                    tokenvalue = modalities.get(int(tokendata,2),int(tokendata,2))
-                    result += prefix + "%s: %s\n"%(tokenname,tokenvalue)
-                elif tokentype == "transition":
-                    tokenvalue = transitions.get(int(tokendata,2),int(tokendata,2))
-                    result += prefix + "%s: %s\n"%(tokenname,tokenvalue)
-                elif tokentype == "network":
-                    tokenvalue = networks.get(int(tokendata,2),int(tokendata,2))
-                    result += prefix + "%s: %s\n"%(tokenname,tokenvalue)
-                elif tokentype == "bcd3":
-                    tokenvalue = ""
-                    tokenvalue += str(int(tokendata[0:4],2))
-                    tokenvalue += str(int(tokendata[4:8],2))
-                    tokenvalue += str(int(tokendata[8:12],2))
-                    result += prefix + "%s: %s\n"%(tokenname,tokenvalue)
-                elif tokentype == "bcddate":
-                    tokenvalue = ""
-                    tokenvalue += str(int(tokendata[0:4],2))
-                    tokenvalue += str(int(tokendata[4:8],2))
-                    tokenvalue += str(int(tokendata[8:12],2))
-                    tokenvalue += str(int(tokendata[12:16],2))
-                    tokenvalue += "-"
-                    tokenvalue += str(int(tokendata[16:20],2))
-                    tokenvalue += str(int(tokendata[20:24],2))
-                    tokenvalue += "-"
-                    tokenvalue += str(int(tokendata[24:28],2))
-                    tokenvalue += str(int(tokendata[28:32],2))
-                    result += prefix + "%s: %s\n"%(tokenname,tokenvalue)
-                elif tokentype == "bestcontract":
-                    #todo : would be better with bin data
-                    # this works here because the size is 24 bits but we should add leading
-                    # zeroes so that mod(len(tokendata),4) == 0
-                    result += parse_hexstring(str(hex(int(tokendata,2)))[2:],best_contract_schema,prefix)
-                elif tokentype == "ascii":
-                    tokenvalue = ""
-                    for i in range(int(tokenlength /8)):
-                        tokenvalue += chr(int(tokendata[i*8:(i+1)*8],2))
-                    result += prefix + "%s: \"%s\"\n"%(tokenname,tokenvalue)
-                elif tokentype == "null":
-                    #check if it's all 0
-                    if int(tokendata,2) == 0:
-                        #result += prefix + "%s: %d 0's\n"% (tokenname,len(tokendata))
-                        result += ""
-                    else:
-                        result += prefix + "%s: WARNING not null : %s\n"% (tokenname,tokendata)
-                else:
-                    result += prefix + "%s: %s\n"%(tokenname,tokendata)
-                #import pdb; pdb.set_trace()
-        except Exception:
-            import traceback
-            traceback.print_exc()
-            print("exception")
-            break
-    return result
-        
-
 
 def format_card(card):
     result =""
     result += ("card <%s> id: %s (%s)\n"% (card['filename'], card['tagid'], card['application-type']))
     result += ("\tdescription:      \"%s\"\n"%card['description'])
     result += ("\tchange-time:      %s\n"%card['change-time'])
-    result += ("\tapplication-data: %s\n"% (card['application-data']))
-    result += ("\t                  %s\n"% (hex2str(card['application-data'])))
-    result += ("\t                  %s\n"% (hex2bin(card['application-data'])))
     files = card['files']
     filelist = files.keys()
     filelist.sort()
@@ -782,12 +540,25 @@ def format_card(card):
         filedesc = filename.get(f,"Unknown")
         result += "_________________________________________________________________\n"
         result += ("\t%s (%s)\n" % (f,filedesc))
-        schem = schema.get(f)
         binschem = file_schemas.get(f)
         for r in files[f]:
             if int(r,16) == 0:
-                result += ("\t\tnull (%d B, %d b)\n"%(int(len(r)/2),len(r)*4))
+                result += ("\t\t000... (%d)\n"%(int(len(r)/2)))
             else:
+                if binschem is not None:
+                    try:
+                        result += "\t\t\\\n"
+                        r2,b = parse_bin(hex2bin(r),binschem,"\t\t| ")
+                        result += r2
+                        if len(b)>0 and int(b,2) != 0:
+                            # some bits remain
+                            result += "\t\t<remainder>" + b + "\n" 
+                    except Exception as e:
+                        print("error for file "+f)
+                        import traceback; traceback.print_exc()
+                else:
+                    result += "\t\t%s\n"%r
+
                 if printhex: result +=  ("\t\t%s\n"%(r))
                 if printstr: result +=  ("\t\t%s\n"%(hex2str(r)))
                 if printbin: result +=  ("\t\t%s\n"%(hex2bin(r)))
@@ -797,15 +568,8 @@ def format_card(card):
                     result +=  ("\t\t%s\n"%(bin2alpha(binstr[1:])))
                     result +=  ("\t\t%s\n"%(bin2alpha(binstr[2:])))
                     result +=  ("\t\t%s\n"%(bin2alpha(binstr[3:])))
-                if schem is not None:
-                    try:
-                        result += parse_hexstring(r,schem,prefix="\t\t\t|")
-                    except:
-                        print("error for file "+f)
-                # new scheme
-                if binschem is not None:
-                    r,b = parse_bin(hex2bin(r),binschem,"\t\t\t>")
-                    result += r
+                    result +=  ("\t\t%s\n"%(bin2alpha(binstr[4:])))
+
     return result
 
 def print_card(card):
